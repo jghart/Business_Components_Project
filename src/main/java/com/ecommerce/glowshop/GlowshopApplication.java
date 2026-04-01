88a627caf9a6275e0526ae8a466a41a10e000655
jghart <appiahjoe88@gmail.com>
2026-04-01 02:53:09 +0000

    Initial Commit

---
+++ src/main/java/com/ecommerce/glowshop/GlowshopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.glowshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowshopApplication.class, args);
+    }
+
+}
