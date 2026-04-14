6a0273973f8a324c300a3217168be9316395a915
RajkamalSingh2002 <rajdhindsa2002@gmail.com>
2026-04-13 22:57:00 -0400

    Made changes in ui and database

--- src/main/java/com/ecommerce/glowshop/controller/AuthController.java
+++ src/main/java/com/ecommerce/glowshop/controller/AuthController.java
@@ -1,10 +1,20 @@
 package com.ecommerce.glowshop.controller;
 
+import com.ecommerce.glowshop.service.CategoryService;
+import com.ecommerce.glowshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/login")
     public String loginPage() {
@@ -12,7 +22,10 @@ public class AuthController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("featuredProducts",
+                productService.getAllProducts().stream().limit(8).toList());
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "index";
     }
 
